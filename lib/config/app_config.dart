45b2c03e0af4d1822eb98b29e7f3ec82021e4e2a
Tuấn Khang <nguyenhuutuankhang412@gmail.com>
2026-01-08 01:11:37 +0700

    refactor: change logo app

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,7 +1,6 @@
 class AppConfig {
-  // static const String baseUrl = 'http://10.0.2.2:8080/api/';
-  static const String baseUrl =
-      'https://bilateral-misunderstandingly-veola.ngrok-free.dev/api/';
+  static const String baseUrl = 'http://10.0.2.2:8080/api/';
+  // static const String baseUrl = 'https://bilateral-misunderstandingly-veola.ngrok-free.dev/api/';
 
   // static const int timeout = 30000;
   // static const bool isDebug = true;
